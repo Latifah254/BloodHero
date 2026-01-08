2d5805f2c31e155ed0af9e07abb1f7c8bed780c0
Latifah254 <latifah.qalbi23@student.uisi.ac.id>
2026-01-08 22:09:08 +0800

    Membuat tampilan login

---
+++ lib/views/login.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'home.dart';
+
+class LoginView extends StatelessWidget {
+  LoginView({super.key});
+
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Login")),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            TextField(
+              controller: emailController,
+              decoration: const InputDecoration(labelText: "Email"),
+            ),
+            TextField(
+              controller: passwordController,
+              obscureText: true,
+              decoration: const InputDecoration(labelText: "Password"),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (_) => HomeView()),
+                );
+              },
+              child: const Text("Login"),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (_) => RegisterView()),
+                );
+              },
+              child: const Text("Belum punya akun? Register"),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
