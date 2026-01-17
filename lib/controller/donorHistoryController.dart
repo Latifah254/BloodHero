d3664fe19a96fefe54ff7d0671244f37879aedae
Latifah254 <latifah.qalbi23@student.uisi.ac.id>
2026-01-18 02:20:46 +0800

    Memperbaiki struktur controller

--- lib/controller/donorHistoryController.dart
+++ lib/controller/donorHistoryController.dart
@@ -1,48 +1,42 @@
 import 'dart:convert';
-import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:bloodhero_app/models/donorHistory.dart';
 
 class DonorHistoryController {
-  static const String baseUrl =
-      "http://10.168.158.36/bloodhero_api";
+  static const String 
+    baseUrl = "http://10.168.158.36/bloodhero_api";
 
-  static Future<bool> addDonor(
-    int userId,
-    String bloodType,
-    String donorDate,
-  ) async {
-    try{
-      final response = await http.post(
+  static Future<bool> addDonor({
+    required int userId,
+    required String donorDate,
+  }) async {
+    final response = await http.post(
       Uri.parse("$baseUrl/api_add_donor.php"),
       body: {
-        "blood_type": bloodType,
+        "user_id": userId.toString(),
         "donor_date": donorDate,
       },
-    ).timeout(const Duration(seconds: 10));
+    );
 
-    final result = jsonDecode(response.body);
-    return result['status'] == 'success';
-  } catch (e) {
-    debugPrint ("ERROR addDonor: $e");
-    return false;
-  }
-
+    try {
+      final data = jsonDecode(response.body);
+      return data["status"] == "success";
+    } catch (_) {
+      return false;
     }
-    
+  }
 
   static Future<List<DonorHistory>> fetchHistory(int userId) async {
     final response = await http.get(
       Uri.parse("$baseUrl/api_donor_history.php?user_id=$userId"),
     );
 
-    
-    final data = jsonDecode(response.body);
+    if (response.statusCode != 200) {
+      throw Exception("Server error");
+    }
 
-    return (data as List)
-          .map((e) => DonorHistory.fromJson(e))
-          .toList();
+    final List data = jsonDecode(response.body);
+
+    return data.map((e) => DonorHistory.fromJson(e)).toList();
   }
-
-  
 }
